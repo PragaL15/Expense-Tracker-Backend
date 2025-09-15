ad4ea9e7735c121b51663db449a1ac7dbc079dd1
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-09-15 20:57:12 +0530

    frontend url is modified

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,19 +18,15 @@ func main() {
 
 	app := fiber.New()
 
-	// Enable CORS
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "https://expense-tracker-frontend-epms.vercel.app", 
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Content-Type, Authorization",
 	}))
-
-	// Simple health check
 	app.Get("/healthz", func(c *fiber.Ctx) error {
 		return c.SendString("ok")
 	})
 
-	// Register routes
 	routes.Register(app)
 
 	addr := fmt.Sprintf(":%s", cfg.Port)
