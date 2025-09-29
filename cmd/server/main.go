14b36432f3d2a83f632513530ac381c95f2ba2eb
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-09-29 20:22:24 +0530

    main is modified

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	app.Use(cors.New(cors.Config{
 		//AllowOrigins: "https://expense-tracker-frontend-one-hazel.vercel.app/", 
-		AllowOrigins: "*",
+		AllowOrigins: "http://localhost:5173",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Content-Type, Authorization",
 	}))
