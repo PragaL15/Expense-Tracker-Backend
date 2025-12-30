3c5e8e61dc1c9521b6eddb447c914beb6114f368
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-12-30 22:34:49 +0530

    budget summary is modified

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,7 +16,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://expense-tracker-frontend-one-hazel.vercel.app/, http://localhost:5173",
+		AllowOrigins: "https://expense-tracker-frontend-one-hazel.vercel.app/",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Content-Type, Authorization",
 	}))
